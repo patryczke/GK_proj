a1bb5c79549f7ffee51c0b4a3437173248a89249
WickeDHypnO <lisonkrystian@gmail.com>
2016-03-23 17:37:53 +0100

    Different ground types, new textures for almost everything

--- Assets/Scripts/Game/CarController.cs
+++ Assets/Scripts/Game/CarController.cs
@@ -8,16 +8,31 @@ public class CarController : MonoBehaviour {
     public float maxSpeed = 20;
     public float turnSpeed = 10;
     Vector3 rotation = new Vector3(0,100,0);
+
+    float actualSpeed;
+    float actualSqrSpeed;
+
+    float defaultAccelerationPower;
+    float defaultBrakingPower;
+    float defaultMaxSpeed;
+    float defaultTurnSpeed;
+
     public GameObject groundCheck;
     // Use this for initialization
     void Start () {
         playersRigidbody = GetComponent<Rigidbody>();
+        defaultMaxSpeed = maxSpeed;
+        defaultTurnSpeed = turnSpeed;
+        defaultBrakingPower = brakingPower;
+        defaultAccelerationPower = accelerationPower;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (isGrounded())
         {
+            actualSpeed = playersRigidbody.velocity.magnitude;
+            actualSqrSpeed = playersRigidbody.velocity.sqrMagnitude;
             if (Input.GetAxis("Vertical") != 0)
             {
                 if (Input.GetAxis("Vertical") > 0)
@@ -41,6 +56,27 @@ public class CarController : MonoBehaviour {
             }
         }
 	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.GetComponent<GroundType>())
+        {
+            if (col.gameObject.GetComponent<GroundType>().type == GroundTypes.Grass)
+            {
+                accelerationPower = defaultAccelerationPower / 8;
+                brakingPower = defaultBrakingPower * 0.9f;
+                turnSpeed = defaultTurnSpeed / 2;
+                maxSpeed = defaultMaxSpeed;
+            }
+            else
+            {
+                accelerationPower = defaultAccelerationPower;
+                brakingPower = defaultBrakingPower;
+                turnSpeed = defaultTurnSpeed;
+                maxSpeed = defaultMaxSpeed;
+            }
+        }
+    }
 
     bool isGrounded()
     {
---
+++ Assets/Scripts/Utility/GroundType.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public enum GroundTypes
+{
+    Road,
+    Grass
+};
+
+public class GroundType : MonoBehaviour {
+    public GroundTypes type;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
