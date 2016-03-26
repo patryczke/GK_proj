036ca1cba65f3e8087944e222bb6e3e34033521e
WickeDHypnO <lisonkrystian@gmail.com>
2016-03-26 17:23:01 +0100

    Added "particle" - smoke, added script to always look at camera, added rocket model

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+    Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+        mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mainCamera != null)
+        {
+            transform.LookAt(mainCamera.transform);
+            transform.forward *= -1;
+        }
+    }
+}
