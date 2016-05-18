7cf5855f5be952a45e90f57a7bb6bdae0e84d74e
jakubmaslon <jakubmaslon@gmail.com>
2016-05-18 20:07:53 +0200

    laps first working version

---
+++ Assets/Scripts/OwnImpls/Checkpoint.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+    public bool check;
+    public Checkpoint previousCheckpoint;
+    public const int numberOfCheckpoints = 4;
+    
+    
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerExit(Collider col) {
+        if (col.gameObject.GetComponent<LapHolder>() && previousCheckpoint.check)
+        {
+            check = true;
+        }
+
+        Checkpoint[] checkpoints = FindObjectsOfType(typeof(Checkpoint)) as Checkpoint[];
+        col.gameObject.GetComponent<LapHolder>().allChecked = true;
+        foreach (Checkpoint checkpoint in checkpoints)
+        {
+            if (!checkpoint.check)
+            {
+                col.gameObject.GetComponent<LapHolder>().allChecked = false;
+            }
+        }
+
+        if (col.gameObject.GetComponent<LapHolder>().allChecked)
+        {
+            col.gameObject.GetComponent<LapHolder>().laps++;
+            foreach (Checkpoint checkpoint in checkpoints)
+            {
+                    checkpoint.check = false;
+            }
+            checkpoints[2].check = true;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/OwnImpls/LapHolder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LapHolder : MonoBehaviour {
+    public float laps = 0;
+    public int counter = 0;
+    public bool allChecked = false;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
